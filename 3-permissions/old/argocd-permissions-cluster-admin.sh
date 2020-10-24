26ccd266387870213d77ffe6239c8cab0c83001e
Paolo Carta <ing.pcarta@gmail.com>
2020-10-24 17:48:51 +0200

    cleanup

--- 3-permissions/old/argocd-permissions-cluster-admin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Example script to add permissions, check the namespaces directory for examples of doing this using YAML
-oc adm policy add-cluster-role-to-user cluster-admin -z argocd-application-controller -n argocd
