4d43593d47a58fdcedf1d8452ec56bed8a1f47c3
Paolo Carta <ing.pcarta@gmail.com>
2021-06-29 10:08:18 +0200

    adapting to new operator version 0.15 and ocp 4.7

--- first-installation.sh
+++ first-installation.sh
@@ -2,8 +2,8 @@
 cd 1.1-argocd-installation-manual-steps
 
 oc apply -f 1-argo-ns.yaml
-oc apply -f 2-argo-og.yaml
-oc apply -f 3-argo-sub.yaml
+# oc apply -f 2-argo-og.yaml
+# oc apply -f 3-argo-sub.yaml
 
 echo Waiting 30 seconds for ArgoCD Operator to come up, if the following step fails with the
 echo error of \"no matches for kind ArgoCD\" then reload the \"5-argo-cluster.yaml\" file manually
